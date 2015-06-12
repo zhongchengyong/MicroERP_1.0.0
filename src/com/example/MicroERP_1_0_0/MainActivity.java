7c0ebbb10edbcc0d897dc9e27681c6a20a531c65
icaniup <zhongcy93@163.com>
2015-06-12 09:49:37 +0800

    2015-04-30

--- src/com/example/MicroERP_1_0_0/MainActivity.java
+++ src/com/example/MicroERP_1_0_0/MainActivity.java
@@ -41,7 +41,6 @@ public class MainActivity extends AbActivity {
         mAbTitleBar.setTitleBarBackground(R.drawable.top_bg);
         mAbTitleBar.setTitleTextMargin(10, 0, 0, 0);
         mAbTitleBar.setLogoLine(R.drawable.line);
-        initTitleRightLayout();
 
         //SlidingMenu的配置
         menu = new SlidingMenu(this);
@@ -80,87 +79,6 @@ public class MainActivity extends AbActivity {
                 startActivity(adviseintent);
             }
         });
-        mBottomTabView = (AbBottomTabView) findViewById(R.id.mBottomTabView);
 
-        //如果里面的页面列表不能下载原因：
-        //Fragment里面用的AbTaskQueue,由于有多个tab，顺序下载有延迟，还没下载好就被缓存了。改成用AbTaskPool，就ok了。
-        //或者setOffscreenPageLimit(0)
-
-        //缓存数量
-        mBottomTabView.getViewPager().setOffscreenPageLimit(5);
-
-        //禁止滑动
-		/*mBottomTabView.getViewPager().setOnTouchListener(new OnTouchListener(){
-
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				return true;
-			}
-
-		});*/
-
-        //mBottomTabView.setOnPageChangeListener(listener);
-
-        SCMFragment page1 = new SCMFragment();
-        HRFragment page2 = new HRFragment();
-        MoreFragment page3=new MoreFragment();
-        AboutFragment page4=new AboutFragment();
-//        FragmentLoad page2 = new FragmentLoad();
-//        FragmentRefresh page3 = new FragmentRefresh();
-//        FragmentLoad page4 = new FragmentLoad();
-
-        List mFragments = new ArrayList<Fragment>();
-        mFragments.add(page1);
-        mFragments.add(page2);
-        mFragments.add(page3);
-        mFragments.add(page4);
-
-        List<String> tabTexts = new ArrayList<String>();
-        tabTexts.add("供应链");
-        tabTexts.add("人力资源");
-        tabTexts.add("更多");
-        tabTexts.add("关于");
-
-        //设置样式
-        mBottomTabView.setTabTextColor(Color.BLACK);
-        mBottomTabView.setTabSelectColor(Color.rgb(255, 255,255));
-        mBottomTabView.setTabBackgroundResource(R.drawable.tab_bg2);
-        mBottomTabView.setTabLayoutBackgroundResource(R.drawable.tablayout_bg2);
-
-        //注意图片的顺序
-        tabDrawables = new ArrayList<Drawable>();
-        tabDrawables.add(this.getResources().getDrawable(R.drawable.menu1_n));
-        tabDrawables.add(this.getResources().getDrawable(R.drawable.menu1_f));
-        tabDrawables.add(this.getResources().getDrawable(R.drawable.menu2_n));
-        tabDrawables.add(this.getResources().getDrawable(R.drawable.menu2_f));
-        tabDrawables.add(this.getResources().getDrawable(R.drawable.menu3_n));
-        tabDrawables.add(this.getResources().getDrawable(R.drawable.menu3_f));
-        tabDrawables.add(this.getResources().getDrawable(R.drawable.menu4_n));
-        tabDrawables.add(this.getResources().getDrawable(R.drawable.menu4_f));
-
-        //演示增加一组
-        mBottomTabView.addItemViews(tabTexts,mFragments,tabDrawables);
-
-        mBottomTabView.setTabPadding(2,10, 2, 2);
-    }
-
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//
-//    }
-
-    private void initTitleRightLayout(){
-
-    }
-
-
-    @Override
-    public void onBackPressed() {
-        if (menu.isMenuShowing()) {
-            menu.showContent();
-        } else {
-            super.onBackPressed();
-        }
     }
 }
